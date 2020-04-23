861e59c20e45fba5660265df6bf2f2aa3e458af0
harrychen23235 <58384208+harrychen23235@users.noreply.github.com>
2020-04-24 01:06:01 +0800

    check detailed infor

--- VisualPytorch/NeuralNetwork/translate/relate_para.py
+++ VisualPytorch/NeuralNetwork/translate/relate_para.py
@@ -5,6 +5,13 @@ Created on Tue Apr 21 16:07:47 2020
 @author: HP
 """
 import numpy as np
+class ModelError(Exception):
+    def __init__(self, erro_info):
+        super().__init__(self)
+        self.error_info = erro_info
+
+    def __str__(self):
+        return self.error_info
 class Global:
     def __init__(self):
         self.s_number=0
@@ -12,7 +19,18 @@ class Global:
         self.dict_number=0
         self.model=np.array([])
         self.para_str={'ceil_mode','count_include_pad','nonlinearity'}
-        
+        self.para_group={'shape','in_channel','out_channel','dim','type','p','layer_type',
+                         'kernel_size','stride','padding','ceil_mode','count_include_pad',
+                         'negative_slope','weight','lower','upper','input_size',
+                         'hidden_size','num_layers','nonlinearity','num_features','num_groups','num_channel'}
+        self.inttype={'in_channel','out_channel','dim','kernel_size','stride','padding','input_size','hidden_size','num_layers',
+                      'num_features','num_groups','num_channel'}
+        self.doubletype={'p','negative_slope','weight','loser','upper'}
+        self.type={'1d','2d','3d'}
+        self.notzero={'in_channel','out_channel','kernel_size','stride','weight','lower','upper','negative_slope',
+                      'input_size','hidden_size','num_layers','num_features','num_groups','num_channel'}
+        self.start_layer = ['start']
+        self.multi_layer = ['element_wise_add_layer', 'concatenate_layer']
 class Node:
 	#记录了单个神经网络层的属性以及连接情况
     def __init__(self, id = None, name = None,data=None,detailed_type=None):

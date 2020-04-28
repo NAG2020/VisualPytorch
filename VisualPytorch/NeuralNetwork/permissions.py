37879e65abd3f812679a9ab7514941c78bd8dd9c
harrychen23235 <58384208+harrychen23235@users.noreply.github.com>
2020-04-29 00:27:07 +0800

    change view.py

--- VisualPytorch/NeuralNetwork/permissions.py
+++ VisualPytorch/NeuralNetwork/permissions.py
@@ -2,4 +2,8 @@ from .models import Network
 
 class ChangeModel(object):
     def has_permission(self, request,view):
-        return True
+        aquire_object = Network.objects.get(pk=view.kwargs['pk'])
+        if request.method == 'GET' or request.user.id == aquire_object.creator_id:
+            return True
+        else:
+            return False

6fc67fe1dc4bee32f619c28682dd74a0032f2e33
Pandapan-Buaa <1009303269@qq.com>
2020-05-15 11:05:39 +0800

    添加邮箱验证功能

--- VisualPytorch/user/urls.py
+++ VisualPytorch/user/urls.py
@@ -6,5 +6,6 @@ from django.urls import include
 urlpatterns = [
     url(r'^register/$', views.UserRegister.as_view()),
     url(r'^login/$',obtain_jwt_token),
-    url(r'^info/$',views.UserInfo.as_view())
+    url(r'^info/$',views.UserInfo.as_view()),
+    url(r'^email/$',views.EmailVariyView.as_view()),
 ]
